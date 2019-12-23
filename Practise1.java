07618384f7570fd6c7802ddbab7bc42ebcbaaca1
ramsaikiran <51018540+ramsaikiran@users.noreply.github.com>
2019-12-23 16:25:09 +0530

    Add files via upload

---
+++ Practise1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class Practise1
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner(System.in);
+String s1=sc.next();
+System.out.println("welcome"+" "+s1);
+}
+}
