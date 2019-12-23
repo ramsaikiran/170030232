cc708ce73798d84e29814e3385594c3f29224a24
ramsaikiran <51018540+ramsaikiran@users.noreply.github.com>
2019-12-23 16:28:19 +0530

    Add files via upload

---
+++ Practise2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+class Practise2
+
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner(System.in);
+int s1=sc.nextInt();
+if(s1%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+
+}
+}
