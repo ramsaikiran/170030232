6439ef2b2b0ec477bb12a67df5d6d3ea6049776f
ramsaikiran <51018540+ramsaikiran@users.noreply.github.com>
2019-12-23 16:22:41 +0530

    Add files via upload

---
+++ Practise.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Practise
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner(System.in);
+String s1=sc.next();
+String s2=sc.next();
+System.out.println(s1+" "+"Technologies"+" "+s2);
+}
+}
